d2a97a399606206f1e1f6af43b78751086d57085
Aratt-Gaytan <60763551+Aratt-Gaytan@users.noreply.github.com>
2023-01-27 18:23:09 -0600

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+class Rectangulo{
+    constructor(_alto, _ancho){
+
+        this.alto = _alto;
+        this.ancho = _ancho;
+    }
+
+    calcularArea(){
+        return this.alto * this.ancho;
+    }
+}
+
+
+function saludar(){
+    alert("buenos dias");
+}
+
+
+function area(){
+    var ancho = document.getElementById("ancho").value;
+    var alto = document.getElementById("alto").value;
+    let rectangulo1 = new Rectangulo(alto, ancho);
+    alert(rectangulo1.calcularArea()); 
+}
---
+++ js/main.js
@@ -0,0 +1,7 @@
+let swlocation = "sw.js";
+
+if(navigator.serviceWorker){
+    if(window.location.href.includes("localhost"))
+        swlocation = "/sw.js";
+    navigator.serviceWorker.register(swlocation);
+}
